0f1495a94a9aa32a26e93d5147345d4a5b9809d9
REDA LAMDABBAR <redalamdabbar@REDAs-MacBook-Pro.local>
2024-04-23 09:19:54 -0700

    lancement branche travail

---
+++ back/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+const port = 3000 || process.env.PORT;
+
+app.listen(port, ()=>{
+console.log("Serveur en cours d'execution ...")
+})
+
+
+// lancement sur branche bdd serveur ladabar
