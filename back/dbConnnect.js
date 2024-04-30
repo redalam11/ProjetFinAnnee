f6d648a81d882e23f79300d5a9638d0d33f90f82
REDA LAMDABBAR <redalamdabbar@REDAs-MacBook-Pro.local>
2024-04-30 12:27:35 -0700

    debut back MVC

--- back/dbConnnect.js
+++ back/dbConnnect.js
@@ -1,5 +1,13 @@
-const { default: mongoose } = require("mongoose")
+const mongoose = require("mongoose");
 
-const dbConnect = () =>{
-    const conn= mongoose.connect('')
+const dbConnect = async () => {
+    try {
+        const uri = 'mongodb+srv://redalamdabbar2004:SoDwPragSVps9S2y@cluster0.rmcvqjn.mongodb.net/'; 
+        const connexionBDD = await mongoose.connect(uri);
+        console.log("BDD connectée avec succès");
+    } catch (error) {
+        console.error("Échec de la connexion à la BDD :", error);
+    }
 }
+
+module.exports = dbConnect;
--- back/index.js
+++ back/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const bddConnect = require("./dbConnnect")
+
 
 
 const port = 3000 || process.env.PORT;
@@ -8,6 +12,8 @@ const port = 3000 || process.env.PORT;
 app.listen(port, ()=>{
 console.log("Serveur en cours d'execution ...")
 })
+
+bddConnect();
 
 app.get("/",(request,response)=>{
     response.send("Bonjour reda lambadabar")
